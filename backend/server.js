d31c8618779dd2758b9850384524e85a4d9d59d4
Akash-urk23cs1243 <makash@karunya.edu.in>
2025-10-31 11:25:41 +0530

    Updated server.js for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,10 @@
 import app from "./app.js";
+import dotenv from "dotenv";
 
-app.listen(process.env.PORT, ()=>{
-    console.log(`SERVER HAS STARTED AT PORT ${process.env.PORT}`);
-})
+dotenv.config();
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`✅ SERVER HAS STARTED AT PORT ${PORT}`);
+});
